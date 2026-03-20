1bac5c05adff28f5ad5f6941dce34f6273030b7e
Jon <jonhasacat8@gmail.com>
2026-03-20 14:33:25 -0700

    Setup reverse proxy for local development.

--- apps/my_vatusa/vite.config.ts
+++ apps/my_vatusa/vite.config.ts
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/my',
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: true,
+    strictPort: true,
+    port: 3002,
+  },
   plugins: [react()],
 })
--- apps/staff/vite.config.ts
+++ apps/staff/vite.config.ts
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/staff',
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: true,
+    strictPort: true,
+    port: 3001,
+  },
   plugins: [react()],
 })
