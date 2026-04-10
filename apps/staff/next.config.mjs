bba3e0cb737086c3e02fec9c7b0fbe4570c5771d
Jon <jonhasacat8@gmail.com>
2026-04-10 13:22:59 -0700

    Hardcode assetPrefix as env variables aren't available at build time.

--- apps/staff/next.config.mjs
+++ apps/staff/next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
-const basePath = process.env.STAFF_NEXT_BASE_PATH ?? ""
 
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
   output: "standalone",
-  assetPrefix: basePath,
+  assetPrefix: "/staff",
 }
 
 export default nextConfig
