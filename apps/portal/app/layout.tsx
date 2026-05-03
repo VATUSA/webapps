fd9dd8ab94bf0c36e497d83bfc970842f8582c35
Celeo <mattboulanger@fastmail.com>
2026-05-02 19:18:20 -0700

    Styling tweaks

--- apps/portal/app/layout.tsx
+++ apps/portal/app/layout.tsx
@@ -39,9 +39,9 @@ export default async function RootLayout({
             <div className="mx-auto w-full max-w-7xl flex-1 px-4 py-6 sm:px-6 lg:px-8">
               {children}
             </div>
-            <Toaster />
             <Footer />
           </div>
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
